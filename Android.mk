52eb7d2ab6c0f0e7a1f510bd587c1096ebaac349
ishubhamsingh <coolsks94@gmail.com>
2016-12-19 10:44:51 +0000

    AEXPapers Bringup Wallpapers designed by @AlienCreature7 Thanks to @SlimRoms for the base app

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016 The SlimRoms Project
+# Copyright (C) 2016 AospExtended ROM Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,5 +19,5 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := SlimWallpapers
+LOCAL_PACKAGE_NAME := AEXPapers
 include $(BUILD_PACKAGE)
