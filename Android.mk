VanillaUnicorn/android_packages_apps_VUPapers

reworked wallpaper app

Android.mk
@@ -1,0 +1,22 @@
+#
+# Copyright (C) 2013 XenonHD
+#
+# Licensed under the GNU GPLv2 license
+#
+# The text of the license can be found in the LICENSE file
+# or at https://www.gnu.org/licenses/gpl-2.0.txt
+#
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES += $(call all-java-files-under, src)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4
+
+LOCAL_PACKAGE_NAME := XenonWallpapers
+
+include $(BUILD_PACKAGE)

src/com/th/XenonWallpapers/SolidColor.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2013 XenonHD
+ *
+ * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package com.th.XenonWallpapers;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import java.io.IOException;
+
+public class SolidColor extends Activity {
+    
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        final CharSequence[] items = getResources().getStringArray(R.array.colors);
+        final Context mContext = this;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.pick_color));
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                int color = Color.BLACK;
+                switch(item){
+                    case 0:
+                        color = Color.parseColor("#CC0000");
+                        break;
+                    case 1:
+                        color = Color.parseColor("#669900");
+                        break;
+                    case 2:
+                        color = Color.parseColor("#0099CC");
+                        break;
+                    case 3:
+                        color = Color.parseColor("#9933CC");
+                        break;
+                    case 4:
+                        color = Color.parseColor("#FF8800");
+                        break;
+                    case 5:
+                        color = Color.WHITE;
+                        break;
+                }
+                
+                try {
+                    WallpaperManager wm = WallpaperManager.getInstance(mContext);
+                    
+                    // Create 1x1 bitmap to store the color
+                    Bitmap bmp = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+
+                    // Make a canvas with which we can draw to the bitmap
+                    Canvas canvas = new Canvas(bmp);
+
+                    // Fill with color and save
+                    canvas.drawColor(color);
+                    canvas.save();
+
+                    wm.setBitmap(bmp);
+                    bmp.recycle();
+                } catch (IOException e) {
+                    // oh lord!
+                }
+                
+                finish();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}

src/com/th/XenonWallpapers/Wallpaper.java
@@ -1,0 +1,232 @@
+/*
+ * Copyright (C) 2013 XenonHD
+ *
+ * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package com.th.XenonWallpapers;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@SuppressLint("ParserError")
+public class Wallpaper extends FragmentActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
+     * sections. We use a {@link android.support.v4.app.FragmentPagerAdapter} derivative, which will
+     * keep every loaded fragment in memory. If this becomes too memory intensive, it may be best
+     * to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+    
+    /**
+     * Menu item used for "Apply" button on actionbar
+     */
+    private static final int MENU_APPLY = Menu.FIRST;
+    
+    /**
+     * The {@link Integer} that stores current fragment selected
+     */
+    private int mCurrentFragment;
+    
+    /**
+     * The {@link ArrayList} that will host the wallpapers resource ID's
+     */
+    static ArrayList <Integer> sWallpapers = new ArrayList<Integer>();
+    
+    /**
+     * The {@link String[]} that will store wallpaper name
+     */
+    String[] mWallpaperInfo;
+    
+    /**
+     * The {@link Context} to be used by the app
+     */
+    static Context mContext;
+
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Create the adapter that will return a fragment for each of the three primary sections
+        // of the app.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOnPageChangeListener(new SimpleOnPageChangeListener(){
+            public void onPageSelected(int position) {
+                mCurrentFragment = position;
+            }
+        });
+        
+        sWallpapers.clear();
+
+        final Resources resources = getResources();
+        final String packageName = getApplication().getPackageName();
+
+        fetchWallpapers(resources, packageName, R.array.wallpapers);
+        mWallpaperInfo = resources.getStringArray(R.array.info);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
+     * sections of the app.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = new WallpaperFragment();
+            Bundle args = new Bundle();
+            args.putInt(WallpaperFragment.ARG_SECTION_NUMBER, i);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return sWallpapers.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mWallpaperInfo[position];
+        }
+    }
+
+    public static class WallpaperFragment extends Fragment {
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            mContext = getActivity();
+            Bundle args = getArguments();
+            LinearLayout holder = new LinearLayout(mContext);
+            holder.setLayoutParams(new LinearLayout.LayoutParams
+                    (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            ImageView img = new ImageView(mContext);
+            img.setLayoutParams(new ViewGroup.LayoutParams
+                    (ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            img.setImageResource(sWallpapers.get(args.getInt(ARG_SECTION_NUMBER)));
+            img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            holder.addView(img);
+            return holder;
+        }
+    }
+
+    private void fetchWallpapers(Resources resources, String packageName, int list) {
+        final String[] extras = resources.getStringArray(list);
+        for (String extra : extras) {
+            int res = resources.getIdentifier(extra, "drawable", packageName);
+            if (res != 0) {
+                sWallpapers.add(res);
+            }
+        }
+    }
+    
+    class WallpaperLoader extends AsyncTask<Integer, Void, Boolean> {
+        BitmapFactory.Options mOptions;
+        ProgressDialog mDialog;
+
+        WallpaperLoader() {
+            mOptions = new BitmapFactory.Options();
+            mOptions.inDither = false;
+            mOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        }
+        
+        @Override
+        protected Boolean doInBackground(Integer... params) {
+            try {
+                Bitmap b = BitmapFactory.decodeResource(getResources(),
+                        sWallpapers.get(params[0]), mOptions);
+                
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
+                try {
+                    wallpaperManager.setBitmap(b);
+                } catch (IOException e) {
+                    // If we crash, we will probably have a null bitmap
+                    // return before recycling to avoid exception
+                    throw new NullPointerException();
+                }
+                
+                // Help GC
+                b.recycle();
+                
+                return true;
+            } catch (OutOfMemoryError e) {
+                return false;
+            } catch(NullPointerException e){
+                return false;
+            }
+        }
+        
+        @Override
+        protected void onPostExecute(Boolean success) {
+            mDialog.dismiss();
+            finish();
+        }
+        
+        @Override
+        protected void onPreExecute() {
+            mDialog = ProgressDialog.show(mContext, null, getString(R.string.applying));
+        }
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_APPLY:
+                new WallpaperLoader().execute(mCurrentFragment);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         menu.add(Menu.NONE, MENU_APPLY, 0, R.string.action_apply)
+                 .setIcon(android.R.drawable.ic_menu_set_as)
+                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+         return super.onCreateOptionsMenu(menu);
+    }
+}

AospExtended/platform_packages_apps_AEXPapers

Android.mk
@@ -1,0 +1,22 @@
+#
+# Copyright (C) 2013 XenonHD
+#
+# Licensed under the GNU GPLv2 license
+#
+# The text of the license can be found in the LICENSE file
+# or at https://www.gnu.org/licenses/gpl-2.0.txt
+#
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES += $(call all-java-files-under, src)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4
+
+LOCAL_PACKAGE_NAME := ValidusWalls
+
+include $(BUILD_PACKAGE)

src/com/gzr/ValidusWalls/SolidColor.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2013 XenonHD
+ *
+ * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package com.gzr.ValidusWalls;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import java.io.IOException;
+
+public class SolidColor extends Activity {
+    
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        final CharSequence[] items = getResources().getStringArray(R.array.colors);
+        final Context mContext = this;
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.pick_color));
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                int color = Color.BLACK;
+                switch(item){
+                    case 0:
+                        color = Color.parseColor("#CC0000");
+                        break;
+                    case 1:
+                        color = Color.parseColor("#669900");
+                        break;
+                    case 2:
+                        color = Color.parseColor("#0099CC");
+                        break;
+                    case 3:
+                        color = Color.parseColor("#9933CC");
+                        break;
+                    case 4:
+                        color = Color.parseColor("#FF8800");
+                        break;
+                    case 5:
+                        color = Color.WHITE;
+                        break;
+                }
+                
+                try {
+                    WallpaperManager wm = WallpaperManager.getInstance(mContext);
+                    
+                    // Create 1x1 bitmap to store the color
+                    Bitmap bmp = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888);
+
+                    // Make a canvas with which we can draw to the bitmap
+                    Canvas canvas = new Canvas(bmp);
+
+                    // Fill with color and save
+                    canvas.drawColor(color);
+                    canvas.save();
+
+                    wm.setBitmap(bmp);
+                    bmp.recycle();
+                } catch (IOException e) {
+                    // oh lord!
+                }
+                
+                finish();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}

src/com/gzr/ValidusWalls/Wallpaper.java
@@ -1,0 +1,232 @@
+/*
+ * Copyright (C) 2013 XenonHD
+ *
+ * Licensed under the GNU GPLv2 license
+ *
+ * The text of the license can be found in the LICENSE file
+ * or at https://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+package com.gzr.ValidusWalls;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.SimpleOnPageChangeListener;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@SuppressLint("ParserError")
+public class Wallpaper extends FragmentActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
+     * sections. We use a {@link android.support.v4.app.FragmentPagerAdapter} derivative, which will
+     * keep every loaded fragment in memory. If this becomes too memory intensive, it may be best
+     * to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+    
+    /**
+     * Menu item used for "Apply" button on actionbar
+     */
+    private static final int MENU_APPLY = Menu.FIRST;
+    
+    /**
+     * The {@link Integer} that stores current fragment selected
+     */
+    private int mCurrentFragment;
+    
+    /**
+     * The {@link ArrayList} that will host the wallpapers resource ID's
+     */
+    static ArrayList <Integer> sWallpapers = new ArrayList<Integer>();
+    
+    /**
+     * The {@link String[]} that will store wallpaper name
+     */
+    String[] mWallpaperInfo;
+    
+    /**
+     * The {@link Context} to be used by the app
+     */
+    static Context mContext;
+
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Create the adapter that will return a fragment for each of the three primary sections
+        // of the app.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOnPageChangeListener(new SimpleOnPageChangeListener(){
+            public void onPageSelected(int position) {
+                mCurrentFragment = position;
+            }
+        });
+        
+        sWallpapers.clear();
+
+        final Resources resources = getResources();
+        final String packageName = getApplication().getPackageName();
+
+        fetchWallpapers(resources, packageName, R.array.wallpapers);
+        mWallpaperInfo = resources.getStringArray(R.array.info);
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
+     * sections of the app.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = new WallpaperFragment();
+            Bundle args = new Bundle();
+            args.putInt(WallpaperFragment.ARG_SECTION_NUMBER, i);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return sWallpapers.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mWallpaperInfo[position];
+        }
+    }
+
+    public static class WallpaperFragment extends Fragment {
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            mContext = getActivity();
+            Bundle args = getArguments();
+            LinearLayout holder = new LinearLayout(mContext);
+            holder.setLayoutParams(new LinearLayout.LayoutParams
+                    (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            ImageView img = new ImageView(mContext);
+            img.setLayoutParams(new ViewGroup.LayoutParams
+                    (ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            img.setImageResource(sWallpapers.get(args.getInt(ARG_SECTION_NUMBER)));
+            img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            holder.addView(img);
+            return holder;
+        }
+    }
+
+    private void fetchWallpapers(Resources resources, String packageName, int list) {
+        final String[] extras = resources.getStringArray(list);
+        for (String extra : extras) {
+            int res = resources.getIdentifier(extra, "drawable", packageName);
+            if (res != 0) {
+                sWallpapers.add(res);
+            }
+        }
+    }
+    
+    class WallpaperLoader extends AsyncTask<Integer, Void, Boolean> {
+        BitmapFactory.Options mOptions;
+        ProgressDialog mDialog;
+
+        WallpaperLoader() {
+            mOptions = new BitmapFactory.Options();
+            mOptions.inDither = false;
+            mOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        }
+        
+        @Override
+        protected Boolean doInBackground(Integer... params) {
+            try {
+                Bitmap b = BitmapFactory.decodeResource(getResources(),
+                        sWallpapers.get(params[0]), mOptions);
+                
+                WallpaperManager wallpaperManager = WallpaperManager.getInstance(mContext);
+                try {
+                    wallpaperManager.setBitmap(b);
+                } catch (IOException e) {
+                    // If we crash, we will probably have a null bitmap
+                    // return before recycling to avoid exception
+                    throw new NullPointerException();
+                }
+                
+                // Help GC
+                b.recycle();
+                
+                return true;
+            } catch (OutOfMemoryError e) {
+                return false;
+            } catch(NullPointerException e){
+                return false;
+            }
+        }
+        
+        @Override
+        protected void onPostExecute(Boolean success) {
+            mDialog.dismiss();
+            finish();
+        }
+        
+        @Override
+        protected void onPreExecute() {
+            mDialog = ProgressDialog.show(mContext, null, getString(R.string.applying));
+        }
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_APPLY:
+                new WallpaperLoader().execute(mCurrentFragment);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         menu.add(Menu.NONE, MENU_APPLY, 0, R.string.action_apply)
+                 .setIcon(android.R.drawable.ic_menu_set_as)
+                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+         return super.onCreateOptionsMenu(menu);
+    }
+}
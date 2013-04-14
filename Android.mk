f6d10ba1e768910c198a750e133820ae2092eebb
Adam80460 <Adam80460@gmail.com>
2013-04-14 11:38:31 -0700

    reworked wallpaper app

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2013 XenonHD
+#
+# Licensed under the GNU GPLv2 license
+#
+# The text of the license can be found in the LICENSE file
+# or at https://www.gnu.org/licenses/gpl-2.0.txt
+#
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES += $(call all-java-files-under, src)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4
+
+LOCAL_PACKAGE_NAME := ValidusWalls
+
+include $(BUILD_PACKAGE)
